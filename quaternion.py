56df2bd6415411af3d84958b6ac0f818ba3c5e16
atrobman <atrobman@vt.edu>
2020-05-18 16:42:19 -0400

    Division added and __str__ added

--- quaternion.py
+++ quaternion.py
@@ -45,58 +45,47 @@ class Quaternion:
 		return Quaternion(na, nb, nc, nd)
 
 	def div(self, comp):
-		# if (comp.a ** 2 + comp.b ** 2) != 0:
-		# 	na = (self.a * comp.a + self.b * comp.b) / (comp.a ** 2 + comp.b ** 2)
-		# 	nb = (self.b * comp.a - self.a * comp.b) / (comp.a ** 2 + comp.b ** 2)
+		"""Source: https://www.mathworks.com/help/aerotbx/ug/quatdivide.html"""
 
-		# 	return Complex(na, nb)
-		# else:
-		# 	raise ZeroDivisionError("division by zero")
+		denom = comp.a ** 2 + comp.b ** 2 + comp.c ** 2 + comp.d ** 2
 
-	def pow(self, comp):
+		if denom != 0:
+			na = (self.a * comp.a + self.b * comp.b + self.c * comp.c + self.d*comp.d) / denom
+			nb = (self.a * comp.b - self.b * comp.a - self.c * comp.d + self.d*comp.c) / denom
+			nc = (self.a * comp.c + self.b * comp.d - self.c * comp.a - self.d*comp.b) / denom
+			nd = (self.a * comp.d - self.b * comp.c + self.c * comp.b - self.d*comp.a) / denom
 
-		# if self.a == 0 and self.b == 0 and comp.a == 0 and comp.b == 0:
-		# 	raise ArithmeticError("indefinite power")
-
-		# elif self.a == 0 and self.b == 0:
-		# 	return Complex(0, 0)
-
-		# elif self.a == 1 and self.b == 0:
-		# 	return Complex(1, 0)
-
-		# elif comp.a == 0 and comp.b == 0:
-		# 	return Complex(1, 0)
-
-		# elif comp.a == 1 and comp.b == 0:
-		# 	return Complex(self.a, self.b)
-
-		# else:
-		# 	mult1 = (self.a ** 2 + self.b ** 2) ** (comp.a / 2) * exp(-comp.b * atan2(self.b, self.a))
-		# 	real_mult = cos(comp.a * atan2(self.b, self.a) + .5 * comp.b * log(self.a**2 + self.b**2))
-		# 	imag_mult = sin(comp.a * atan2(self.b, self.a) + .5 * comp.b * log(self.a**2 + self.b**2))
-
-		# 	na = mult1 * real_mult
-		# 	nb = mult1 * imag_mult
-
-		# 	return Complex(na, nb)
+			return Quaternion(na, nb, nc, nd)
+		else:
+			raise ZeroDivisionError("division by zero")
 
 	def __str__(self):
-
-		# msg = ""
-		# if self.a != 0:
-		# 	msg += f"{self.a} "
-
-		# if self.b != 0:
-		# 	if self.b > 0 and self.a != 0:
-		# 		msg += f"+ {self.b}*i"
-		# 	elif self.b < 0 and self.a != 0:
-		# 		msg += f"- {self.b*-1}*i"
-		# 	elif self.b > 0 and self.a == 0:
-		# 		msg += f"{self.b}*i"
-		# 	elif self.b < 0 and self.a == 0:
-		# 		msg += f"{self.b}*i"
-
-		# elif msg == "":
-		# 		msg = "0"
-
-		# return msg
+		msg = ""
+		if self.a != 0:
+			msg += f"{self.a}"
+		
+		if self.c != 0:
+			if msg == "":
+				msg += f"{self.c}*j"
+			else:
+				
+				if self.c > 0:
+					msg += f"+ {self.c}*j"
+				else:
+					msg += f"- {self.c*-1}*j"
+		
+		if self.d != 0:
+			if msg == "":
+				msg += f"{self.d}*k"
+			else:
+				
+				if self.d > 0:
+					msg += f"+ {self.d}*k"
+				else:
+					msg += f"- {self.d*-1}*k"
+		
+		elif msg == "":
+			msg = "0"
+		
+		return msg
+		
