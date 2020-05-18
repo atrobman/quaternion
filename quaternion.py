952663ba8a5d8fd73f1e342d2e634987e1b309c1
atrobman <atrobman@vt.edu>
2020-05-18 18:36:59 -0400

    Integration with int/float and exp/ln functions

--- quaternion.py
+++ quaternion.py
@@ -1,4 +1,6 @@
-from math import floor, sqrt
+from math import floor, sqrt, cos, sin, acos
+from math import exp as math_exp
+from math import log as math_ln
 
 class Quaternion:
 
@@ -10,54 +12,91 @@ class Quaternion:
 		self.d = d
 
 	def mul(self, comp):
-		""" Distributing [a1+b1*i+c1*j+d1*k] * [a2+b2*i+c2*j+d2*k] =
-			a1*a2 + a1*b2*i + a1*c2*j + a1*d2*k +
-			b1*a2*i + b1*b2*i^2 + b1*c2*i*j + b1*d2*i*k +
-			c1*a2*j + c1*b2*j*i + c1*c2*j^2 + c1*d2*j*k +
-			d1*a2*k + d1*b2*k*i + d1*b2*j*k + d1*d2*k^2 =
 
-			a1*a2 - b1*b2 - c1*c2 - d1*d2 +
-		   (a1*b2 + b1*a2 + c1*d2 - d1*c2) * i +
-		   (a1*c2 - b1*d2 + c1*a2 + d1*b2) * j +
-		   (a1*d2 + b1*c2 - c1*b2 + d1*a2) * k
-		"""
-		na = self.a * comp.a - self.b * comp.b - self.c * comp.c - self.d * comp.d
-		nb = self.a * comp.b + self.b * comp.a + self.c * comp.d - self.d * comp.c
-		nc = self.a * comp.c - self.b * comp.d + self.c * comp.a + self.d * comp.b
-		nd = self.a * comp.d + self.b * comp.c - self.c * comp.b + self.d * comp.a
+		if type(comp) is Quaternion:
+			""" Distributing [a1+b1*i+c1*j+d1*k] * [a2+b2*i+c2*j+d2*k] =
+				a1*a2 + a1*b2*i + a1*c2*j + a1*d2*k +
+				b1*a2*i + b1*b2*i^2 + b1*c2*i*j + b1*d2*i*k +
+				c1*a2*j + c1*b2*j*i + c1*c2*j^2 + c1*d2*j*k +
+				d1*a2*k + d1*b2*k*i + d1*b2*j*k + d1*d2*k^2 =
 
-		return Quaternion(na, nb, nc, nd)
-
-	def add(self, comp):
-		na = self.a + comp.a
-		nb = self.b + comp.b
-		nc = self.c + comp.c
-		nd = self.d + comp.d
-
-		return Quaternion(na, nb, nc, nd)
-
-	def sub(self, comp):
-		na = self.a - comp.a
-		nb = self.b - comp.b
-		nc = self.c - comp.c
-		nd = self.d - comp.d
-
-		return Quaternion(na, nb, nc, nd)
-
-	def div(self, comp):
-		"""Source: https://www.mathworks.com/help/aerotbx/ug/quatdivide.html"""
-
-		denom = comp.a ** 2 + comp.b ** 2 + comp.c ** 2 + comp.d ** 2
-
-		if denom != 0:
-			na = (comp.a * self.a + comp.b * self.b + comp.c * self.c + comp.d * self.d) / denom
-			nb = (comp.a * self.b - comp.b * self.a - comp.c * self.d + comp.d * self.c) / denom
-			nc = (comp.a * self.c + comp.b * self.d - comp.c * self.a - comp.d * self.b) / denom
-			nd = (comp.a * self.d - comp.b * self.c + comp.c * self.b - comp.d * self.a) / denom
+				a1*a2 - b1*b2 - c1*c2 - d1*d2 +
+			(a1*b2 + b1*a2 + c1*d2 - d1*c2) * i +
+			(a1*c2 - b1*d2 + c1*a2 + d1*b2) * j +
+			(a1*d2 + b1*c2 - c1*b2 + d1*a2) * k
+			"""
+			na = self.a * comp.a - self.b * comp.b - self.c * comp.c - self.d * comp.d
+			nb = self.a * comp.b + self.b * comp.a + self.c * comp.d - self.d * comp.c
+			nc = self.a * comp.c - self.b * comp.d + self.c * comp.a + self.d * comp.b
+			nd = self.a * comp.d + self.b * comp.c - self.c * comp.b + self.d * comp.a
 
 			return Quaternion(na, nb, nc, nd)
+		
+		elif type(comp) is int or type(comp) is float:
+			return Quaternion(self.a * comp, self.b * comp, self.c * comp, self.d * comp)
+		
 		else:
-			raise ZeroDivisionError("division by zero")
+			raise TypeError(f'Expected type={type(self)} but recieved type={type(comp)}')
+
+	def add(self, comp):
+
+		if type(comp) is Quaternion:
+			na = self.a + comp.a
+			nb = self.b + comp.b
+			nc = self.c + comp.c
+			nd = self.d + comp.d
+
+			return Quaternion(na, nb, nc, nd)
+		
+		elif type(comp) is int or type(comp) is float:
+			return Quaternion(self.a + comp, self.b, self.c, self.d)
+		
+		else:
+			raise TypeError(f'Expected type={type(self)} but recieved type={type(comp)}')
+
+	def sub(self, comp):
+
+		if type(comp) is Quaternion:
+			na = self.a - comp.a
+			nb = self.b - comp.b
+			nc = self.c - comp.c
+			nd = self.d - comp.d
+
+			return Quaternion(na, nb, nc, nd)
+
+		elif type(comp) is int or type(comp) is float:
+			return Quaternion(self.a - comp, self.b, self.c, self.d)
+		
+		else:
+			raise TypeError(f'Expected type={type(self)} but recieved type={type(comp)}')
+
+	def div(self, comp):
+
+		if type(comp) is Quaternion:
+			"""Source: https://www.mathworks.com/help/aerotbx/ug/quatdivide.html"""
+
+			denom = comp.a ** 2 + comp.b ** 2 + comp.c ** 2 + comp.d ** 2
+
+			if denom != 0:
+				na = (comp.a * self.a + comp.b * self.b + comp.c * self.c + comp.d * self.d) / denom
+				nb = (comp.a * self.b - comp.b * self.a - comp.c * self.d + comp.d * self.c) / denom
+				nc = (comp.a * self.c + comp.b * self.d - comp.c * self.a - comp.d * self.b) / denom
+				nd = (comp.a * self.d - comp.b * self.c + comp.c * self.b - comp.d * self.a) / denom
+
+				return Quaternion(na, nb, nc, nd)
+			else:
+				raise ZeroDivisionError("division by zero")
+		
+		elif type(comp) is int or type(comp) is float:
+
+			if comp != 0:
+				return Quaternion(self.a / comp, self.b / comp, self.c / comp, self.d / comp)
+			else:
+				raise ZeroDivisionError("division by zero")
+		
+		else:
+			raise TypeError(f'Expected type={type(self)} but recieved type={type(comp)}')
+
 	
 	def norm(self):
 		return sqrt( self.a ** 2 + self.b ** 2 + self.c ** 2 + self.d ** 2 )
@@ -66,15 +105,25 @@ class Quaternion:
 		return Quaternion(self.a, -1*self.b, -1*self.c, -1*self.d)
 	
 	def unit(self):
-		norm = self.norm()
-		
-		return Quaternion(self.a / norm, self.b / norm, self.c / norm, self.d / norm)
+		return self / self.norm()
 	
 	def inverse(self):
-		norm_2 = self.norm() ** 2
-		conj = self.conj()
+		return self.conj() / (self.norm() ** 2)
 
-		return Quaternion(conj.a / norm_2, conj.b / norm_2, conj.c / norm_2, conj.d / norm_2)
+	def exp(self):
+		v = Quaternion(0, self.b, self.c, self.d)
+
+		norm_v = v.norm()
+		unit_v = v.unit()
+
+		rhs = unit_v * sin(norm_v) + cos(norm_v)
+
+		return  rhs * math_exp(self.a)
+
+	def ln(self):
+		v = Quaternion(0, self.b, self.c, self.d)
+
+		return v.unit() * acos(self.a / self.norm()) + math_ln(self.norm())
 
 	def __mul__(self, comp):
 		return self.mul(comp)
